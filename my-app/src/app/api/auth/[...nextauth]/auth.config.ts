fd312ff103b4671d08bc464cce6147914b1f9e9b
paradimes <anas.mohmand@uoit.net>
2024-12-09 19:54:08 -0500

    cleanup

--- my-app/src/app/api/auth/[...nextauth]/auth.config.ts
+++ my-app/src/app/api/auth/[...nextauth]/auth.config.ts
@@ -1,20 +1,11 @@
 // src/app/api/auth/[...nextauth]/auth.config.ts
+
 import { AuthOptions } from "next-auth";
-import Auth0 from "next-auth/providers/auth0";
 import { prisma } from "@/lib/prisma";
-// import { getOrCreateUser } from "@/lib/auth";
 import Google from "next-auth/providers/google";
 
 export const authOptions: AuthOptions = {
   providers: [
-    Auth0({
-      clientId: process.env.AUTH_AUTH0_ID as string,
-      clientSecret: process.env.AUTH_AUTH0_SECRET as string,
-      issuer: process.env.AUTH_AUTH0_ISSUER,
-      authorization: {
-        url: `https://${process.env.AUTH_AUTH0_ISSUER}/authorize?response_type=code&prompt=login`,
-      },
-    }),
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
@@ -47,20 +38,6 @@ export const authOptions: AuthOptions = {
         console.error("Error during sign in:", error);
         return false;
       }
-
-      // if (account?.provider === "auth0") {
-      //   try {
-      //     await getOrCreateUser({
-      //       id: user.id,
-      //       email: user.email!,
-      //     });
-      //     return true;
-      //   } catch (error) {
-      //     console.error("Error creating user:", error);
-      //     return false;
-      //   }
-      // }
-      // return true;
     },
     async session({ session, token }) {
       if (session.user) {
--- my-app/src/components/auth/AuthButton.tsx
+++ my-app/src/components/auth/AuthButton.tsx
@@ -2,28 +2,18 @@
 "use client";
 
 import { useSession, signIn, signOut } from "next-auth/react";
-// import { useRouter } from "next/navigation";
 
 export default function AuthButton() {
   const { data: session, status } = useSession();
-  // const router = useRouter();
 
   if (status === "loading") {
     return <div>Loading...</div>;
   }
 
-  // const handleLogout = () => {
-  //   const returnTo = encodeURI(String(window.location.href));
-  //   const authURL = `${process.env.NEXT_PUBLIC_AUTH0_ISSUER}/v2/logout?client_id=${process.env.NEXT_PUBLIC_AUTH0_ID}&returnTo=${returnTo}`;
-  //   router.push(authURL);
-  //   signOut();
-  // };
-
   if (session) {
     return (
       <div>
         <button
-          // onClick={handleLogout}
           onClick={() => signOut()}
           className=" hover:bg-neutral-100 dark:hover:bg-neutral-800 p-2 rounded"
         >
